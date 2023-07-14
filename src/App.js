4a33ee8f67b8750f77fde6b3d52b54ba14ac537b
nttruongqn <nttruongqn@gmail.com>
2023-07-14 16:59:38 +0700

    hook useImperativeHandle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Video from './Video';
+import { useRef } from 'react';
 
 function App() {
+  const videoRef = useRef()
+
+  const handlePlay = () => {
+    videoRef.current.play()
+  }
+
+  const handlePause = () => {
+    videoRef.current.pause()
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Video ref= {videoRef}/>
+      <button onClick={handlePlay}>Play</button>
+      <button onClick={handlePause}>Pause</button>
     </div>
   );
 }
